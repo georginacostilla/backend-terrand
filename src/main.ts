d6341f34a372303500569b30168ec7269465b3b0
georginacostilla <georginacostilla.gc@gmail.com>
2025-04-30 23:18:43 -0300

    configuraciones iniciales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestApplication, NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { ClassSerializerInterceptor, Logger } from '@nestjs/common';
+import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalInterceptors(new LoggerInterceptor());
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') || 3000;
+  const NODE_ENV = configService.get<string>('NODE_ENV');
+
+  await app.listen(PORT, () => {
+    Logger.log(
+      `Application running the port: http://localhost:${PORT}`,
+      NestApplication.name,
+    );
+    Logger.log(`Current Environment: ${NODE_ENV}`, NestApplication.name);
+  });
+
 }
+
 bootstrap();
