7772032e46139e1f87a7322186e1ca9d69ebc2c4
georginacostilla <georginacostilla.gc@gmail.com>
2025-05-01 01:25:33 -0300

    correción de código

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestApplication, NestFactory, Reflector } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
-import { ClassSerializerInterceptor, Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 import { LoggerInterceptor } from './common/interceptors/logger.interceptor';
 import { setupSwagger } from './config/swagger.config';
 
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new LoggerInterceptor());
-  
+
   setupSwagger(app);
 
   const configService = app.get(ConfigService);
